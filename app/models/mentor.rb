a75e56d70ac61a87f0a514eaef8b91d86b8e131c
liliabula <liliabula@gmail.com>
2017-03-26 21:56:57 +0100

    added validation to each mentor submission page field and profile image

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -2,4 +2,9 @@ class Mentor < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :cover, styles: { medium: "300x300>", thumb: "100x100>" }
   	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
+
+	validates :name, :profession, :location, :description, :price, :industries, :expertise, presence: true
+	validates :price, numericality: { greater_than: 0 }
+	validates :cover, attachment_presence: true
+
 end
